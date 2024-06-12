1af315b4378ac98184c201a96d9ababb8cbc8775
esavaner <44378917+esavaner@users.noreply.github.com>
2024-06-13 00:48:10 +0200

    Nest.js fundamentals - chapter 5

--- nest.js/fundamentals/src/app.module.ts
+++ nest.js/fundamentals/src/app.module.ts
@@ -10,11 +10,11 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres', // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database host
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'postgres', // name of our database,
+      host: process.env.DATABASE_HOST, // database host
+      port: +process.env.DATABASE_PORT, // database host
+      username: process.env.DATABASE_USERNAME, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database,
       autoLoadEntities: true, // models will be loaded automatically
       synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
     }),
